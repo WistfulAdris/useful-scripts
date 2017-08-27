c0c0de86b21e395c2d86b6085338c7ac0bf59b26
Irvin <simdol@users.noreply.github.com>
2017-08-27 17:48:34 +0900

    nnp.ssh: bump nginx naxsi and pagespeed to the latest version

--- scripts/nginx-naxsi-pagespeed/nnp.sh
+++ scripts/nginx-naxsi-pagespeed/nnp.sh
@@ -3,9 +3,9 @@
 #This is the variables for the build. Make sure to change them before running the script.
 DIRECTORY=/root/webserver
 #Do NOT add '/', trailing slash in the end.
-PAGESPEED=1.11.33.4
-NGINX=1.11.9
-NAXSI=0.55.1
+PAGESPEED=1.12.34.2
+NGINX=1.10.3
+NAXSI=0.55.3
 COMPILE="--add-module=${DIRECTORY}/ngx_pagespeed-${PAGESPEED}-beta \
 --add-module=${DIRECTORY}/naxsi-${NAXSI}/naxsi_src \
 --without-mail_pop3_module \
